d2348ac242ce6c6f29b86dcbcfb12202ba7bc0d2
ApurboShib <shibjoy088@gmail.com>
2026-02-27 21:19:06 +0600

    feat: Add dynamic routes for profiles and author details, replacing a static profile route.

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ app.get("/profile", (req, res) => {
     res.render("index");
 })
 
-app.get("/profile/joy", (req, res) => {
-    res.send("<script src='/javascripts/script.js'></script>");
+app.get("/profile/:username", (req, res) => {
+    // parrams means je kon route er aghe collon ache.
+    req.params.username;
+    //res.send("<script src='/javascripts/script.js'></script>");
+    res.send(`welcome ${req.params.username}`)
 })
 
+app.get("/author/:username/:age", (req, res) => {
+    // res.send("hello from new routes");
+    res.send(`${req.params.username} age is ${req.params.age} `);
+})
 
 app.listen(port, () => {
     console.log(`the is running on port :  ${port}`);
